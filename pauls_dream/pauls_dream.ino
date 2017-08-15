0eaf42172d73270cf7ae3839fc936cbfcdc661fa
hate <ih4t3youall@gmail.com>
2017-08-15 17:52:20 -0300

    falta terminar

--- pauls_dream/pauls_dream.ino
+++ pauls_dream/pauls_dream.ino
@@ -1,34 +1,34 @@
 int pir= 2;
+int pirLiving =3;
+
 int relee = 13;
-
-int pin2 =2;
-int pin3=3;
-int pin4=4;
-int pin5=5;
-int pin6=6;
-
+int releeLiving =12;
 int pieza = 1;
+int living =1;
 
-const int led = 13;  // the pin with a LED
-int ledState = LOW;    // El LED empieza apagado
 unsigned long T1;
+unsigned long T2;
 int INTERVALO =15000;
+int INTERVALOLIVING = 30000;
 int flag = false;
 
      void setup(){
 	pinMode(pir, INPUT);
+	pinMode(pirLiving,fINPUT);
+
+	pinMode(releeLiving,OUTPUT);
 	pinMode(relee, OUTPUT); 
 	for (int i = 7 ; i < 12 ; i++)
         	 pinMode(i, INPUT) ;
 	Serial.begin(9600);
-     }
-
+     } 
 
 
     void loop(void) {
-
-        int pire = digitalRead(pir);
+        int pire= digitalRead(pir);
+        int pire = digitalRead(pireLiving);
         readRemote();
+	checkLiving();
         if(pieza == 1){
 
           if(pire == HIGH){
@@ -54,8 +54,13 @@ int flag = false;
    int readRemote(){
      if (digitalRead(7) ){
 	    Serial.print("Valid trans. \t");
-            if (digitalRead(8)) 
-                Serial.print( "Boton B, pulsado");
+            if (digitalRead(8)){ 
+		living++;
+			if(living == 4){
+				living =1;
+			}
+		delay(500);
+	    }
             if (digitalRead(9)) 
                 Serial.print( "Boton D, pulsado");
             if (digitalRead(10)) 
@@ -76,3 +81,30 @@ int flag = false;
             Serial.println("\t");
          } 
     }
+
+
+void checkLiving(){
+
+
+        if(living == 1){
+
+          if(pire == HIGH){
+             digitalWrite(releeLiving,HIGH);
+             T1 = millis();
+           }
+
+           if (millis() - T2 > INTERVALOLIVING) {
+             digitalWrite(releeLiving,LOW);
+           }
+
+        }
+	
+	if(living == 2){ 
+            digitalWrite(releeLiving,LOW);
+        }
+
+	if(living == 3){ 
+            digitalWrite(releeLiving,HIGH);
+        }
+
+}
